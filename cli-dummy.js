457848662f63b5aa29f90373699fc3188a7c05ef
hnunezz <nunesdealmeidahenrique@gmail.com>
2023-06-01 22:18:41 -0300

    ##

--- cli-dummy.js
+++ cli-dummy.js
@@ -1,17 +1,3 @@
-/*
-  Usage
- 
- node {file}.js <command> <-flags> <--options[=value]> <arguments>
- 
- Example:
-    node cli-dummy.js
-    node cli-dummy.js help
-    node cli-dummy.js hello
-    node cli-dummy.js hello -a
-    node cli-dummy.js hello --to={name}
-    node cli-dummy.js hello -a --to={name}
-*/
-
 //?Invocação da linha de comando -- Retira-se os dois comandos padroes (node && cli-dummy.js)
 //?A partir disso a const 'argv' passa receber os commands/flags/arguments
 const argv = process.argv.slice(2);
@@ -76,29 +62,3 @@ function hello(toAll, toPerson) {
 }
 
 //! ------------------------------------------------
-// Por exemplo, se você executar seu script com:
-
-// node app.js arthur 35
-// você pode obter esses valores de uma maneira mais legível como esta:
-
-// const [node, script, name, age] = process.argv;
-
-// console.log(node); // node
-// console.log(script); // app.js
-// console.log(name); // arthur
-// console.log(age); // 35
-// Você pode omitir o primeiro e o segundo lugares de your process.argve ficar apenas com namee age:
-
-// const [, , name, age] = process.argv;
-// Se você quiser todos os argumentos em um array, você pode fazer isso usando o rest syntax, que coleta vários elementos e os condensa em um único elemento como este:
-
-// const [node, script, ...params] = process.argv;
-
-// console.log(node); // node
-// console.log(script); // app.js
-// console.log(params); // [ 'arthur', '35' ]
-// e, claro, você pode omitir o primeiro e o segundo lugares do seu process.argve ficar apenas com seus parâmetros :
-
-// const [, , ...params] = process.argv;
-
-//Ref https://stackoverflow.com/questions/22213980/could-someone-explain-what-process-argv-means-in-node-js-please
